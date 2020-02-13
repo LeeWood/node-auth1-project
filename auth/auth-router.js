59be11867bc6d306ebdd879ca58f861e96c29035
lee-wood <leemaewood@gmail.com>
2020-02-12 23:54:55 -0500

    login feature added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,4 +19,24 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Logged In! Welcome back ${user.username}!`});
+      } else {
+        res.status(401).json({ message: "You shall not PASS!!"});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an issue logging you in.",
+        error: err
+      });
+    });
+});
+
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -4,7 +4,6 @@ function find() {
   return db('users').select('id', 'username');
 }
 
-
 function add(user) {
   return db('users')
     .insert(user, 'id')
@@ -21,7 +20,14 @@ function findById(id) {
     .first();
 }
 
+function findBy(filter) {
+  return db('users')
+    .select('id', 'username', 'password')
+    .where(filter);
+}
+
 module.exports = {
   find,
+  findBy,
   add
 }
