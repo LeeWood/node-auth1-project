5fa72b61a44201a6a591007c7ad369060dc5b9e2
lee-wood <leemaewood@gmail.com>
2020-02-12 23:19:32 -0500

    register feature, password is hashed before saved to db

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const apiRouter = require('./api-router.js');
 const server = express();
 
+server.use(express.json());
 server.use('/api', apiRouter);
 
 module.exports = server;
--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,27 @@
 const db = require('../db/db-config.js');
 
-function findUser() {
+function find() {
   return db('users').select('id', 'username');
 }
 
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db('users')
+    .select('id', 'username')
+    .where({ id })
+    .first();
+}
+
 module.exports = {
-  findUser
+  find,
+  add
 }
