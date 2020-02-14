d7377659228dec6d51163a79f7eefe6eb5166c56
lee-wood <leemaewood@gmail.com>
2020-02-14 18:13:07 -0500

    fixed endpoint to match README

--- api/api-router.js
+++ api/api-router.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
-router.use('/auth', authRouter);
+router.use('/', authRouter);
+// router.use('/register', authRouter);
 router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
