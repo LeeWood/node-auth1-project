c158cba80eb995652bd4ec1f9af83152b8864d18
lee-wood <leemaewood@gmail.com>
2020-02-15 12:45:47 -0500

    session data now being saved to database

--- api/server.js
+++ api/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const apiRouter = require('./api-router.js');
 const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
 const server = express();
 
 const sessionConfig = {
   name: "dRudman",
   secret: "Treehouse hangout, no boys allowed.",
   cookie: {
-    maxAge: 600000, //10 minute session.
+    maxAge: 1000 * 60 * 20, //20 minute session.
     secure: false,
     httpOnly: true,
   },
   resave: false,
-  saveUninitialized: true //should be false in production.
+  saveUninitialized: true, //should be false in production.
+  store: new knexSessionStore({
+    knex: require('../db/db-config.js'),
+    tablename: 'sessions',
+    sidfieldname: 'SID',
+    createtable: true,
+    clearInterval: 1000 * 60 * 20 //20 mins
+  }),
 };
 
 server.use(express.json());
