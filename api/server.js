a8fc580ba9a70bc64276866482133869b09e0bb2
lee-wood <leemaewood@gmail.com>
2020-02-14 22:03:24 -0500

    cookie saved when user logs in, restricted middeware now getting validation from cookies

--- api/server.js
+++ api/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const apiRouter = require('./api-router.js');
+const session = require('express-session');
 const server = express();
 
+const sessionConfig = {
+  name: "dRudman",
+  secret: "Treehouse hangout, no boys allowed.",
+  cookie: {
+    maxAge: 600000, //10 minute session.
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true //should be false in production.
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 server.use('/api', apiRouter);
 
 module.exports = server;
--- middleware/restrictedAuth.js
+++ middleware/restrictedAuth.js
@@ -1,30 +1,11 @@
-const bcrypt = require('bcryptjs');
-const Users = require('../users/users-model');
 
 function restrictedAuth(req, res, next) {
-  const { username, password } = req.headers;
-
-  if(username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if(user && bcrypt.compareSync(password, user.password)) {
-          next();
-        }else {
-          res.status(401).json({
-            message: "You shall not PASS!"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: "There was an issue logging you in",
-          error: err
-        });
-      })
+  
+  if(req.session && req.session.user) {
+    next();
   }else {
-    res.status(400).json({
-      message: 'Please enter username and password.'
+    res.status(401).json({
+      message: 'Log in to view this page.'
     });
   }
 };
